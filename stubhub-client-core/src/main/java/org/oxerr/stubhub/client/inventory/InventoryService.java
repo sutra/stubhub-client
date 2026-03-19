757fe905552a5ed37be26b12e7b9a9d01f092c9a
Joe Zhou <zhoushuqun@gmail.com>
2026-03-20 02:19:08 +0800

    Add method streamInventories.

--- stubhub-client-core/src/main/java/org/oxerr/stubhub/client/inventory/InventoryService.java
+++ stubhub-client-core/src/main/java/org/oxerr/stubhub/client/inventory/InventoryService.java
@@ -1,17 +1,27 @@
 package org.oxerr.stubhub.client.inventory;
 
 import java.util.Iterator;
+import java.util.Spliterators;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 import org.oxerr.stubhub.client.model.ListingResponse;
 
 public interface InventoryService {
 
-	/**
-	 * Iterate inventories.
-	 */
 	default Iterator<ListingResponse> iterateInventories() {
 		return iterateInventories(new InventorySearchCriteria());
 	}
 
 	Iterator<ListingResponse> iterateInventories(InventorySearchCriteria criteria);
+
+	default Stream<ListingResponse> streamInventories() {
+		return streamInventories(new InventorySearchCriteria());
+	}
+
+	default Stream<ListingResponse> streamInventories(InventorySearchCriteria criteria) {
+		var i = iterateInventories(criteria);
+		return StreamSupport.stream(Spliterators.spliteratorUnknownSize(i, 0), false);
+	}
+
 }
