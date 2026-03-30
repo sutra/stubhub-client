6c84770335b71930d726d2484ab3e2b20582cd81
Joe Zhou <zhoushuqun@gmail.com>
2026-03-31 00:37:38 +0800

    Add header Account-Id.

--- stubhub-client-cxf/stubhub-client-cxf-impl/src/main/java/org/oxerr/stubhub/client/cxf/impl/filter/BearerAuthFilter.java
+++ stubhub-client-cxf/stubhub-client-cxf-impl/src/main/java/org/oxerr/stubhub/client/cxf/impl/filter/BearerAuthFilter.java
@@ -1,20 +1,27 @@
 package org.oxerr.stubhub.client.cxf.impl.filter;
 
+import java.io.IOException;
+import java.util.UUID;
+
 import jakarta.ws.rs.client.ClientRequestContext;
 import jakarta.ws.rs.client.ClientRequestFilter;
-import java.io.IOException;
 
 public class BearerAuthFilter implements ClientRequestFilter {
 
+	private final UUID accountId;
+
 	private final String apiKey;
 
-	public BearerAuthFilter(String apiKey) {
+	public BearerAuthFilter(UUID accountId, String apiKey) {
+		this.accountId = accountId;
 		this.apiKey = apiKey;
 	}
 
 	@Override
 	public void filter(ClientRequestContext requestContext) throws IOException {
-		requestContext.getHeaders().add("Authorization", "Bearer " + apiKey);
+		var headers = requestContext.getHeaders();
+		headers.add("Account-Id", accountId);
+		headers.add("Authorization", "Bearer " + apiKey);
 	}
 
 }
