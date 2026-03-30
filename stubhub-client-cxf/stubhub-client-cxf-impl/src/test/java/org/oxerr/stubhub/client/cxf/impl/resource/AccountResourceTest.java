a6677e9929e9ef71962825a74e698575fd1beb87
Joe Zhou <zhoushuqun@gmail.com>
2026-03-31 01:19:12 +0800

    Add AccountResourceTest.

---
+++ stubhub-client-cxf/stubhub-client-cxf-impl/src/test/java/org/oxerr/stubhub/client/cxf/impl/resource/AccountResourceTest.java
@@ -0,0 +1,33 @@
+package org.oxerr.stubhub.client.cxf.impl.resource;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.oxerr.stubhub.client.cxf.impl.CXFStubHubClient;
+import org.oxerr.stubhub.client.cxf.impl.CXFStubHubClients;
+import org.oxerr.stubhub.client.cxf.resource.AccountResource;
+import org.oxerr.stubhub.client.model.SellerAccountResponse;
+
+class AccountResourceTest {
+
+	private final CXFStubHubClient client = CXFStubHubClients.getClient();
+
+	private final AccountResource accountResource = client.getAccountResource();
+
+	@Test
+	void testGet() {
+		/*
+		-Djava.util.logging.manager=org.apache.logging.log4j.jul.LogManager
+		-Dcom.sun.xml.ws.transport.http.client.HttpTransportPipe.dump=true
+		-Dcom.sun.xml.internal.ws.transport.http.client.HttpTransportPipe.dump=true
+		-Dcom.sun.xml.ws.transport.http.HttpAdapter.dump=true
+		-Dcom.sun.xml.internal.ws.transport.http.HttpAdapter.dump=true
+		*/
+
+		SellerAccountResponse sa = accountResource.get().get(0);
+		assertNotNull(sa.getSellerAccountId());
+		assertTrue(sa.getSellerAccountName().startsWith("sandbox."));
+	}
+
+}
