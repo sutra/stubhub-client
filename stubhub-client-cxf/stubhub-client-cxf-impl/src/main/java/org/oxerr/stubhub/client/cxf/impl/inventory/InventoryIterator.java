83cc4575e406f53cb7658c3fb88025e9dee9037a
Joe Zhou <zhoushuqun@gmail.com>
2026-03-31 11:30:44 +0800

    Polish code.

--- stubhub-client-cxf/stubhub-client-cxf-impl/src/main/java/org/oxerr/stubhub/client/cxf/impl/inventory/InventoryIterator.java
+++ stubhub-client-cxf/stubhub-client-cxf-impl/src/main/java/org/oxerr/stubhub/client/cxf/impl/inventory/InventoryIterator.java
@@ -2,7 +2,6 @@ package org.oxerr.stubhub.client.cxf.impl.inventory;
 
 import java.util.Collections;
 import java.util.Iterator;
-import java.util.List;
 import java.util.Optional;
 
 import org.apache.logging.log4j.LogManager;
