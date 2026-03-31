eaddacebf25b50d3c9c17b59cb5f9e0dc2ae0a1e
Joe Zhou <zhoushuqun@gmail.com>
2026-03-31 21:36:17 +0800

    Revert "Add log."
    This reverts commit a6b422bb72cb863afdaffeb05fab8b4e8c1be1c4.

--- stubhub-client-cxf/stubhub-client-cxf-impl/src/main/java/org/oxerr/stubhub/client/cxf/impl/filter/BearerAuthFilter.java
+++ stubhub-client-cxf/stubhub-client-cxf-impl/src/main/java/org/oxerr/stubhub/client/cxf/impl/filter/BearerAuthFilter.java
@@ -3,15 +3,10 @@ package org.oxerr.stubhub.client.cxf.impl.filter;
 import java.io.IOException;
 import java.util.UUID;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 import jakarta.ws.rs.client.ClientRequestContext;
 import jakarta.ws.rs.client.ClientRequestFilter;
 
 public class BearerAuthFilter implements ClientRequestFilter {
-
-	private final Logger log = LogManager.getLogger();
 
 	private final UUID accountId;
 
@@ -27,9 +22,6 @@ public class BearerAuthFilter implements ClientRequestFilter {
 		var headers = requestContext.getHeaders();
 		headers.add("Account-Id", accountId);
 		headers.add("Authorization", "Bearer " + apiKey);
-
-		log.trace("Added headers: Account-Id={}, Authorization=Bearer {}", accountId, apiKey);
-
 	}
 
 }
