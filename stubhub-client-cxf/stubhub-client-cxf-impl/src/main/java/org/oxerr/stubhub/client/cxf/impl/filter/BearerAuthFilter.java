a6b422bb72cb863afdaffeb05fab8b4e8c1be1c4
Joe Zhou <zhoushuqun@gmail.com>
2026-03-31 20:42:47 +0800

    Add log.

--- stubhub-client-cxf/stubhub-client-cxf-impl/src/main/java/org/oxerr/stubhub/client/cxf/impl/filter/BearerAuthFilter.java
+++ stubhub-client-cxf/stubhub-client-cxf-impl/src/main/java/org/oxerr/stubhub/client/cxf/impl/filter/BearerAuthFilter.java
@@ -3,10 +3,15 @@ package org.oxerr.stubhub.client.cxf.impl.filter;
 import java.io.IOException;
 import java.util.UUID;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import jakarta.ws.rs.client.ClientRequestContext;
 import jakarta.ws.rs.client.ClientRequestFilter;
 
 public class BearerAuthFilter implements ClientRequestFilter {
+
+	private final Logger log = LogManager.getLogger();
 
 	private final UUID accountId;
 
@@ -22,6 +27,9 @@ public class BearerAuthFilter implements ClientRequestFilter {
 		var headers = requestContext.getHeaders();
 		headers.add("Account-Id", accountId);
 		headers.add("Authorization", "Bearer " + apiKey);
+
+		log.trace("Added headers: Account-Id={}, Authorization=Bearer {}", accountId, apiKey);
+
 	}
 
 }
