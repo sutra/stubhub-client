0b2f3cb6a6088ecb30e81f8108a80e00c12d0721
Joe Zhou <zhoushuqun@gmail.com>
2026-04-11 14:59:41 +0800

    Implement ListingNoteRequest equals & hashCode, toString.

---
+++ stubhub-client-cxf/stubhub-client-cxf-impl/src/test/java/org/oxerr/stubhub/client/cxf/impl/inventory/UpdateInventoryTest.java
@@ -0,0 +1,23 @@
+package org.oxerr.stubhub.client.cxf.impl.inventory;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.jupiter.api.Test;
+import org.oxerr.stubhub.client.cxf.impl.CXFStubHubClient;
+import org.oxerr.stubhub.client.cxf.impl.CXFStubHubClients;
+import org.oxerr.stubhub.client.model.ListingResponse;
+
+class UpdateInventoryTest {
+
+	private static final Logger log = LogManager.getFormatterLogger();
+
+	private final CXFStubHubClient client = CXFStubHubClients.getClient();
+
+	@Test
+	void testUpdateInventory() {
+		long inventoryId = 939796293L;
+		ListingResponse listing = client.inventory().resource().getInventory(inventoryId, false).get(0);
+		log.info("listing: %s", listing);
+	}
+
+}
