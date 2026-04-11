858c0d8f0f9219e9028f503f8c3321fd3e6e6ff5
Joe Zhou <zhoushuqun@gmail.com>
2026-04-11 16:59:38 +0800

    Add support to construct CXFStubHubClient with HTTPClientPolicy.

--- stubhub-client-cxf/stubhub-client-cxf-impl/src/main/java/org/oxerr/stubhub/client/cxf/impl/CXFStubHubClient.java
+++ stubhub-client-cxf/stubhub-client-cxf-impl/src/main/java/org/oxerr/stubhub/client/cxf/impl/CXFStubHubClient.java
@@ -2,8 +2,11 @@ package org.oxerr.stubhub.client.cxf.impl;
 
 import java.util.List;
 import java.util.UUID;
+import java.util.function.Consumer;
 
-import org.apache.cxf.jaxrs.client.JAXRSClientFactory;
+import org.apache.cxf.jaxrs.client.ClientConfiguration;
+import org.apache.cxf.transport.http.HTTPConduit;
+import org.apache.cxf.transports.http.configuration.HTTPClientPolicy;
 import org.oxerr.stubhub.client.StubHubClient;
 import org.oxerr.stubhub.client.cxf.impl.event.CXFEventService;
 import org.oxerr.stubhub.client.cxf.impl.filter.BearerAuthFilter;
@@ -23,11 +26,9 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.jakarta.rs.json.JacksonJsonProvider;
 
+import jakarta.annotation.Nullable;
+
 public class CXFStubHubClient implements StubHubClient {
-
-	private final String baseAddress;
-
-	private final List<?> providers;
 
 	private final AccountResource accountResource;
 
@@ -43,23 +44,50 @@ public class CXFStubHubClient implements StubHubClient {
 
 	private final CXFInventoryService inventoryService;
 
-	public CXFStubHubClient(UUID accountId, String apiKey) {
-		this.baseAddress = "https://pointofsaleapi.stubhub.net";
+	public CXFStubHubClient(UUID accountId, String token) {
+		this(accountId, token, null, config -> {});
+	}
+
+	public CXFStubHubClient(
+		UUID accountId,
+		String token,
+		@Nullable HTTPClientPolicy policy
+	) {
+		this(accountId, token, policy, config -> {});
+	}
+
+	public CXFStubHubClient(
+		UUID accountId,
+		String token,
+		@Nullable HTTPClientPolicy policy,
+		Consumer<ClientConfiguration> configurer
+	) {
+		var baseAddress = "https://pointofsaleapi.stubhub.net";
+
+		Consumer<ClientConfiguration> internalConfigurer = config -> {
+			if (policy != null) {
+				HTTPConduit conduit = (HTTPConduit) config.getConduit();
+				conduit.setClient(policy);
+			}
+			configurer.accept(config);
+		};
 
 		JacksonJsonProvider jacksonJsonProvider = createJacksonJsonProvider();
-		BearerAuthFilter authFilter = new BearerAuthFilter(accountId, apiKey);
-		this.providers = List.of(
+		BearerAuthFilter authFilter = new BearerAuthFilter(accountId, token);
+		List<?> providers = List.of(
 			jacksonJsonProvider,
 			authFilter
 		);
 
-		accountResource = createProxy(AccountResource.class);
-		currencyConversionOverrideResource = createProxy(CurrencyConversionOverrideResource.class);
-		dealResource = createProxy(DealResource.class);
-		healthCheckResource = createProxy(HealthCheckResource.class);
-		holdResource = createProxy(HoldResource.class);
-		eventService = new CXFEventService(createProxy(EventResource.class));
-		inventoryService = new CXFInventoryService(createProxy(InventoryResource.class));
+		var f = new ClientFactory(baseAddress, providers, internalConfigurer);
+
+		accountResource = f.create(AccountResource.class);
+		currencyConversionOverrideResource = f.create(CurrencyConversionOverrideResource.class);
+		dealResource = f.create(DealResource.class);
+		healthCheckResource = f.create(HealthCheckResource.class);
+		holdResource = f.create(HoldResource.class);
+		eventService = new CXFEventService(f.create(EventResource.class));
+		inventoryService = new CXFInventoryService(f.create(InventoryResource.class));
 	}
 
 	public AccountResource getAccountResource() {
@@ -80,14 +108,6 @@ public class CXFStubHubClient implements StubHubClient {
 
 	public HoldResource getHoldResource() {
 		return holdResource;
-	}
-
-	protected <T> T createProxy(Class<T> cls) {
-		return JAXRSClientFactory.create(
-			baseAddress,
-			cls,
-			providers
-		);
 	}
 
 	protected JacksonJsonProvider createJacksonJsonProvider() {
