7ae2a26ca9d2a74d9bcf6d332fa36bf5479d573a
Joe Zhou <zhoushuqun@gmail.com>
2026-04-13 01:14:31 +0800

    Update price checker.

--- stubhub-client-cxf/stubhub-client-cxf-impl/src/test/java/org/oxerr/stubhub/client/cxf/impl/inventory/UpdateInventoryTest.java
+++ stubhub-client-cxf/stubhub-client-cxf-impl/src/test/java/org/oxerr/stubhub/client/cxf/impl/inventory/UpdateInventoryTest.java
@@ -1,11 +1,16 @@
 package org.oxerr.stubhub.client.cxf.impl.inventory;
 
+import java.util.List;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.oxerr.stubhub.client.cxf.impl.CXFStubHubClient;
 import org.oxerr.stubhub.client.cxf.impl.CXFStubHubClients;
+import org.oxerr.stubhub.client.model.ApiMarketplace;
+import org.oxerr.stubhub.client.model.InventoryPriceUpdateRequest;
+import org.oxerr.stubhub.client.model.InventoryUpdateRequest;
 import org.oxerr.stubhub.client.model.ListingResponse;
 
 class UpdateInventoryTest {
@@ -17,9 +22,34 @@ class UpdateInventoryTest {
 	@Disabled("Requires authentication")
 	@Test
 	void testUpdateInventory() {
-		long inventoryId = 939796293L;
+		long inventoryId = 901619532L;
 		ListingResponse listing = client.inventory().resource().getInventory(inventoryId, false).get(0);
 		log.info("listing: %s", listing);
+
+		var request = createInventoryUpdateRequest(listing);
+		log.info("request: %s", request);
+		var response = client.inventory().resource().updateInventory(inventoryId, request);
+
+		log.info("response: %s", response);
+	}
+
+	private InventoryUpdateRequest createInventoryUpdateRequest(ListingResponse inventory) {
+		var request = new InventoryUpdateRequest();
+		request.setPrices(List.of(createInventoryPriceUpdateRequest(inventory)));
+		return request;
+	}
+
+	private InventoryPriceUpdateRequest createInventoryPriceUpdateRequest(ListingResponse inventory) {
+		var request = new InventoryPriceUpdateRequest();
+
+		request.setMarketplace(ApiMarketplace.STUB_HUB);
+
+		// Payload: {"code":"bad_request","message":null,"errors":{"Prices[0]":["Can only update one of the following AllInPrice or ListPrice or MarketplaceMarkup"]}}
+		// request.setAllInPrice(inventory.getUnitCost());
+
+		request.setListPrice(inventory.getUnitCost());
+
+		return request;
 	}
 
 }
