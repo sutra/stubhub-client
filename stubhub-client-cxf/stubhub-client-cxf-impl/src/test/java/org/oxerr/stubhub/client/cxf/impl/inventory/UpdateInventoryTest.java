f48391bac4d087fc9837700e051ea553ab7fcd82
Joe Zhou <zhoushuqun@gmail.com>
2026-04-12 22:24:39 +0800

    Disable tests.

--- stubhub-client-cxf/stubhub-client-cxf-impl/src/test/java/org/oxerr/stubhub/client/cxf/impl/inventory/UpdateInventoryTest.java
+++ stubhub-client-cxf/stubhub-client-cxf-impl/src/test/java/org/oxerr/stubhub/client/cxf/impl/inventory/UpdateInventoryTest.java
@@ -2,6 +2,7 @@ package org.oxerr.stubhub.client.cxf.impl.inventory;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.oxerr.stubhub.client.cxf.impl.CXFStubHubClient;
 import org.oxerr.stubhub.client.cxf.impl.CXFStubHubClients;
@@ -13,6 +14,7 @@ class UpdateInventoryTest {
 
 	private final CXFStubHubClient client = CXFStubHubClients.getClient();
 
+	@Disabled("Requires authentication")
 	@Test
 	void testUpdateInventory() {
 		long inventoryId = 939796293L;
