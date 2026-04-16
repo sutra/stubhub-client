a8815f99907bba97a7c61d13462d6dc87315c683
Joe Zhou <zhoushuqun@gmail.com>
2026-04-17 02:41:07 +0800

    Add method to invoke /inventory/export/lean.

---
+++ stubhub-client-model/src/main/java/org/oxerr/stubhub/client/model/ListingLeanResponse.java
@@ -0,0 +1,179 @@
+package org.oxerr.stubhub.client.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public class ListingLeanResponse {
+
+	private UUID sellerAccountId;
+
+	private Long listingId;
+
+	private Integer quantity;
+
+	private String section;
+
+	private String row;
+
+	private BigDecimal cost;
+
+	private BigDecimal price;
+
+	private String externalId;
+
+	private Integer eventId;
+
+	private String stockType;
+
+	private String listingNotes;
+
+	private String internalNotes;
+
+	private String broadcastStatus;
+
+	private String stubHubListingId;
+
+	private String eventName;
+
+	private String venueName;
+
+	private LocalDateTime eventDate;
+
+	public UUID getSellerAccountId() {
+		return sellerAccountId;
+	}
+
+	public void setSellerAccountId(UUID sellerAccountId) {
+		this.sellerAccountId = sellerAccountId;
+	}
+
+	public Long getListingId() {
+		return listingId;
+	}
+
+	public void setListingId(Long listingId) {
+		this.listingId = listingId;
+	}
+
+	public Integer getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+
+	public String getSection() {
+		return section;
+	}
+
+	public void setSection(String section) {
+		this.section = section;
+	}
+
+	public String getRow() {
+		return row;
+	}
+
+	public void setRow(String row) {
+		this.row = row;
+	}
+
+	public BigDecimal getCost() {
+		return cost;
+	}
+
+	public void setCost(BigDecimal cost) {
+		this.cost = cost;
+	}
+
+	public BigDecimal getPrice() {
+		return price;
+	}
+
+	public void setPrice(BigDecimal price) {
+		this.price = price;
+	}
+
+	public String getExternalId() {
+		return externalId;
+	}
+
+	public void setExternalId(String externalId) {
+		this.externalId = externalId;
+	}
+
+	public Integer getEventId() {
+		return eventId;
+	}
+
+	public void setEventId(Integer eventId) {
+		this.eventId = eventId;
+	}
+
+	public String getStockType() {
+		return stockType;
+	}
+
+	public void setStockType(String stockType) {
+		this.stockType = stockType;
+	}
+
+	public String getListingNotes() {
+		return listingNotes;
+	}
+
+	public void setListingNotes(String listingNotes) {
+		this.listingNotes = listingNotes;
+	}
+
+	public String getInternalNotes() {
+		return internalNotes;
+	}
+
+	public void setInternalNotes(String internalNotes) {
+		this.internalNotes = internalNotes;
+	}
+
+	public String getBroadcastStatus() {
+		return broadcastStatus;
+	}
+
+	public void setBroadcastStatus(String broadcastStatus) {
+		this.broadcastStatus = broadcastStatus;
+	}
+
+	public String getStubHubListingId() {
+		return stubHubListingId;
+	}
+
+	public void setStubHubListingId(String stubHubListingId) {
+		this.stubHubListingId = stubHubListingId;
+	}
+
+	public String getEventName() {
+		return eventName;
+	}
+
+	public void setEventName(String eventName) {
+		this.eventName = eventName;
+	}
+
+	public String getVenueName() {
+		return venueName;
+	}
+
+	public void setVenueName(String venueName) {
+		this.venueName = venueName;
+	}
+
+	public LocalDateTime getEventDate() {
+		return eventDate;
+	}
+
+	public void setEventDate(LocalDateTime eventDate) {
+		this.eventDate = eventDate;
+	}
+
+}
