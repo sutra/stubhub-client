182f0d08b9708a61ee06164e4c702d83f305d803
Joe Zhou <zhoushuqun@gmail.com>
2026-04-20 00:03:32 +0800

    Add ExportTest.

---
+++ stubhub-client-cxf/stubhub-client-cxf-impl/src/test/java/org/oxerr/stubhub/client/cxf/impl/inventory/ExportTest.java
@@ -0,0 +1,22 @@
+package org.oxerr.stubhub.client.cxf.impl.inventory;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.oxerr.stubhub.client.cxf.impl.CXFStubHubClient;
+import org.oxerr.stubhub.client.cxf.impl.CXFStubHubClients;
+
+class ExportTest {
+
+	private static final Logger log = LogManager.getFormatterLogger();
+
+	private final CXFStubHubClient client = CXFStubHubClients.getClient();
+
+	@Disabled("Requires authentication")
+	@Test
+	void testExport() {
+		var count = client.inventory().streamInventories().count();
+		log.info("count: %d", count);
+	}
+}
