efeacc2fbcab1d7a7bc1ff0024b9ded075fcf5a4
Joe Zhou <zhoushuqun@gmail.com>
2026-04-23 18:56:33 +0800

    Add InventoryExportLeanResponse.json and InvoiceExportResponse.json.

--- stubhub-client-cxf/stubhub-client-cxf-impl/src/test/java/org/oxerr/stubhub/client/cxf/impl/invoice/InvoiceExportTest.java
+++ stubhub-client-cxf/stubhub-client-cxf-impl/src/test/java/org/oxerr/stubhub/client/cxf/impl/invoice/InvoiceExportTest.java
@@ -1,11 +1,15 @@
 package org.oxerr.stubhub.client.cxf.impl.invoice;
 
+import java.util.Comparator;
+import java.util.concurrent.atomic.AtomicInteger;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.oxerr.stubhub.client.cxf.impl.CXFStubHubClient;
 import org.oxerr.stubhub.client.cxf.impl.CXFStubHubClients;
+import org.oxerr.stubhub.client.model.SaleResponse;
 
 class InvoiceExportTest {
 
@@ -16,12 +20,18 @@ class InvoiceExportTest {
 	@Disabled("Requires authentication")
 	@Test
 	void testExport() {
-		client.invoice().streamInvoices().forEach(s -> {
-			log.info("Invoice: %s %s %,.2f - %,.2f = %,.2f",
-				s.getSaleDate(), s.getId(),
-				s.getTotalNetProceeds(), s.getTotalCost(),
-				s.getTotalNetProceeds().subtract(s.getTotalCost()));
-		});
+		AtomicInteger counter = new AtomicInteger();
+		client.invoice()
+			.streamInvoices()
+			.sorted(Comparator.comparing(SaleResponse::getCreatedDate))
+			.forEach(s -> {
+				log.info("Invoice %d: %s(%s) %s %,.2f - %,.2f = %,.2f",
+					counter.incrementAndGet(),
+					s.getSaleDate(), s.getEvent().getDate(),
+					s.getId(),
+					s.getTotalNetProceeds(), s.getTotalCost(),
+					s.getTotalNetProceeds().subtract(s.getTotalCost()));
+			});
 	}
 
 }
