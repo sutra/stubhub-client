c31931fa2d41f4c21a24fea4bee938e9c276be58
Joe Zhou <zhoushuqun@gmail.com>
2026-05-10 01:12:40 +0800

    Print marketplace fullfilment state.

--- stubhub-client-cxf/stubhub-client-cxf-impl/src/test/java/org/oxerr/stubhub/client/cxf/impl/invoice/InvoiceExportTest.java
+++ stubhub-client-cxf/stubhub-client-cxf-impl/src/test/java/org/oxerr/stubhub/client/cxf/impl/invoice/InvoiceExportTest.java
@@ -3,6 +3,7 @@ package org.oxerr.stubhub.client.cxf.impl.invoice;
 import java.util.Comparator;
 import java.util.concurrent.atomic.AtomicInteger;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -26,14 +27,18 @@ class InvoiceExportTest {
 			.streamInvoices()
 			.sorted(Comparator.comparing(SaleResponse::getCreatedDate))
 			.forEach(s -> {
+				int count = counter.incrementAndGet();
 				Level level = s.getCreatedDate().isAfter(s.getEvent().getDate()) ? Level.WARN : Level.INFO;
-				log.log(level, "Invoice %d: %s(%s) %s(%s) eventId=%s eventName=%s %,.2f - %,.2f = %,.2f",
-					counter.incrementAndGet(),
-					s.getSaleDate(), s.getEvent().getDate(),
+				log.log(level,
+					"Invoice %s: %s(%s) %s %s(%s) eventId=%s eventName=%s %,.2f - %,.2f = %,.2f",
+					StringUtils.leftPad(String.valueOf(count), 4, '0'),
 					s.getId(), s.getMarketplaceSaleId(),
+					StringUtils.rightPad(s.getMarketplaceState().getFullfilmentState(), 9),
+					s.getSaleDate(), s.getEvent().getDate(),
 					s.getEvent().getId(), s.getEventMapping().getEventName(),
 					s.getTotalNetProceeds(), s.getTotalCost(),
-					s.getTotalNetProceeds().subtract(s.getTotalCost()));
+					s.getTotalNetProceeds().subtract(s.getTotalCost())
+				);
 			});
 	}
 
